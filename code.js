1e7141c8f68bfe4ccc2b14b625a5db6e99e85772
IamLAM <ingeniero@luismonge.com.mx>
2019-05-24 23:28:00 -0700

    background color change js

---
+++ code.js
@@ -0,0 +1,25 @@
+
+ 
+ function changeColor(){
+    console.log("entro");
+  /*  $("html body").animate(
+        {
+          backgroundColor: 'red',
+          color: 'white'
+        },
+        1000
+      );
+*/
+$('body').css('background-color', '#FF4000');
+$('text').css('background-color', '#FF4000');
+
+}
+
+
+ 
+ $(document).ready(function() {
+  //  console.log("entro");
+  $('#new-quote').on('click', changeColor);
+  }); 
+
+
